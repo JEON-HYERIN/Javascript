88d5897b681107a2023c2b45b5602168ce665630
JEON-HYERIN <jeonhyelin19951118@gmail.com>
2021-08-02 11:51:58 +0900

    Chapter3-4 Hoisting

--- js/main.js
+++ js/main.js
@@ -1,17 +1,10 @@
-// 즉시실행함수
-// IIFE, Immediately-Invoked Function Expression
+// 호이스팅(Hoisting)
+// 함수 선언부가 유효범위 최상단으로 끌어올려지는 현상
 
-const a = 7
+const a = 7;
+
+double();
+
 function double() {
   console.log(a * 2)
 }
-double();
-
-(function () {
-  console.log(a * 2)
-})();
-
-
-(function () {
-  console.log(a * 2)
-}());
