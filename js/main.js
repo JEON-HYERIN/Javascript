24eac4e8c3a9b629c95c7b7c84345b770bd12e14
JEON-HYERIN <jeonhyelin19951118@gmail.com>
2021-08-02 12:41:30 +0900

    Chapter3-6  callback

--- js/main.js
+++ js/main.js
@@ -1,32 +1,39 @@
-//타이머 함수
-// setTimeout(함수, 시간): 일정 시간 후 함수 실행
-// setInterval(함수, 시간): 시간 간격마다 함수 실행
-// clearTimeout(): 설정된 Timeout 함수 종료
-// clearInterval(): 설정된 Interval 함수 종료
+// 콜백(Callbak)
+// 함수의 인수로 사용되는 함수
 
-//  setTimeout(function () {
-//   console.log('Hello!');
-// }, 3000);
+// setTimeout(함수, 시간)
 
-// setTimeout(() => {
-//   console.log('Hello!');
-// }, 3000);
 
-// const timer = setTimeout(() => {
-//   console.log('Hello!');
-// }, 3000);
+// function timeout(cb) {
+//   setTimeout(() => {
+//     console.log('hello!')
+//     cb();
+//   }, 3000);
+// };
 
-// const h1El = document.querySelector('h1');
-// h1El.addEventListener('click', () => {
-//   clearTimeout(timer);
+// timeout(() => {
+//   console.log('Done!');
 // });
+function timeout(cb) {
+  setTimeout(function () { {
+    console.log('hello!')
+    cb();
+  }, 3000);
+}
+};
 
-
-const timer = setInterval(() => {
-  console.log('Hello!');
-}, 3000);
-
-const h1El = document.querySelector('h1');
-h1El.addEventListener('click', () => {
-  clearInterval(timer);
+timeout(() => {
+  console.log('Done!');
 });
+
+
+// function timeout(cb) {
+//   setTimeout(function() {
+//     console.log('hello');
+//     cb();
+//   }, 3000);
+// };
+
+// timeout(function() {
+//   console.log(Done!);
+// });
