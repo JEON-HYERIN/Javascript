7ce2f8d715dd5953be84ba66e735a03317a5bab8
JEON-HYERIN <jeonhyelin19951118@gmail.com>
2021-08-06 11:20:48 +0900

    Chapter3-1 함수복습 arguments

--- js/main.js
+++ js/main.js
@@ -1,27 +1,27 @@
-// 형 변환(Type conversion)
+// 함수 복습
 
-// Truthy(참 같은 값)
-// true, {}, [], 1, 2, 'false', -12, '3.14' ...
+// function sum(x ,y) {
+//   return x + y
+//    함수 내부에서 return 키워드 사용시 return 키워드 뒤에있는 어떠한 내용이 함수 밖으로
+//    반환된다는 것을 의미하며 또한, 추가적으로 
+//    return 키워드가 사용된 그 부분에서 함수가 종료된다는 것을 의미
+//   따라서 그 이하 내용들은 실행되지 않음
+// }
 
-// Falsy(거짓 같은 값)
-// false, '', null, undefined, 0, -0, NaN(Not a Number, 숫자데이터임)
-// NaN = 1 + undefined 더해진값이 숫자로 판단할 수 없는 특정 값일 경우
-// 숫자데이터이긴한데 숫자가아니다 라는 뜻으로 NaN 이 있음
+// const a = sum(1, 3)
+// const b = sum(4, 12)
+// console.log(a)
+// console.log(b)
+// console.log(a + b)
 
-if ('false') {
-  console.log(123)
+
+function sum() {
+  console.log(arguments)
+  return arguments[0] + arguments[1]
 }
+//  함수 안에서는 매개변수를 지정하지 않아도 arguments라는 특정 객체 사용 가능
+//  arguments라는 객체는 함수안에서 언제든지 사용가능
+//  함수 실행할때 인수로 넣은 값이 배열형태로 들어가짐
+// 하지만 직관적이지 않으므로 되도록 사용하지 않기를 권장
 
-// const a = 1
-// const b = '1'
-
-// console.log(a === b)
-// // 일치연산자
-
-
-// console.log(a == b)
-// 동등연산자
-// 동등연산자 사용시 형변환이 일어남
-// 자바스크립트에서는 동등연산자 되도록 사용하지 말것
-// 의도하지 않은 결과가 나올 수 있음
-// 따라서 === 일치연산자 사용 권장
+console.log(sum(7, 3))
