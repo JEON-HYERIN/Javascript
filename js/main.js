ecd4aaeee5675c7a6e5945f723aa7aa3e36e13fc
JEON-HYERIN <jeonhyelin19951118@gmail.com>
2021-08-06 15:24:18 +0900

    Chapter3-4 호이스팅

--- js/main.js
+++ js/main.js
@@ -1,19 +1,13 @@
-// 즉시실행함수
-// IIFE, Immediately-Invoked Function Expression
+// 호이스팅(Hoisting)
+// 함수 선언부가 유효범위 최상단으로 끌어올려지는현상, 표현x 선언o
 
 const a = 7
 
+double()
+
 function double() {
   console.log(a * 2)
-};
-double();
+}
 
-// 1번
-(function () {
-  console.log(a * 2)
-})();
 
-// 2번 - 권장
-(function () {
-  console.log(a * 2)
-}());
+
