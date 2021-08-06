1f82855e886d35e8bce477f9b06b4dfb87697f5d
JEON-HYERIN <jeonhyelin19951118@gmail.com>
2021-08-06 16:23:09 +0900

    Chapter3-6  콜백

--- js/main.js
+++ js/main.js
@@ -1,27 +1,16 @@
-// 타이머함수
-// setTimeout(함수, 시간): 일정 시간 후 함수 실행
-// setInterval(함수, 시간): 시간 간격마다 함수 실행
-// 시간부분에서 사용되는 단위 ms , 1000ms = 1초
-// clearTimeout(): 설정된 Timeout 함수 종료
-// clearInterval(): 설정된 Interval 함수 종료
+// 콜백(Callback)
+// 함수의 인수로 사용되는 함수
 
-const timer = setTimeout(() => {
-  console.log('hello')
-}, 3000)
+// setTimeout(함수, 시간)
 
-const h1El = document.querySelector('h1')
 
-h1El.addEventListener('click', () => {
-  clearTimeout(timer)
+function timeout(callback) {
+  setTimeout(() => {
+    console.log('hello')
+    callback()
+  }, 3000)
+}
+
+timeout(() => {
+  console.log('world')
 })
-
-
-// const timer = setInterval(() => {
-//   console.log('hello')
-// }, 3000)
-
-// const h1El = document.querySelector('h1')
-
-// h1El.addEventListener('click', () => {
-//   clearInterval(timer)
-// })
