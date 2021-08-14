6b2a852e54a8976af66f7bd6da35db5938b4993a
JEON-HYERIN <jeonhyelin19951118@gmail.com>
2021-08-14 18:18:59 +0900

    Chapter4-6 구조 분해 할당

--- js/main.js
+++ js/main.js
@@ -1,80 +1,97 @@
-// prototype 속성이 붙어있지 않는 메소드 : 정적(static)메소드
-// {}.assign() 중괄호를 사용하는 객체 리터럴부분에 메소드를 직접적으로 사용불가
-
-
-// Object.assign()
-// 1번째 인수로 들어가져있는 타켓이라는 객체에 2번째에 인수로 들어가는 객체 병합해서 병합된 타겟의 객체를 반환
-// 객체데이터에서 속성의 이름은 고유해야됨
-
-const userAge = {
-  // Key: value
+// 구조 분해 할당(Destructuring assignment)
+// 또는 비구조화 할당
+// 구조 분해 할당이라는 것은 user라는 객체데이터에서 내용을 구조 분해해서 내가 원하는 속성들만 꺼내서 사용
+// const나 let 키워드를 사용해서 구조분해된 내용을 변수로 만들어 활용
+// 이런방식으로 객체데이터를 구조분해해서 각각의 변수로 만들어 활용 
+// 이러한 방식으로 객체데이터를 const나 let 키워드를 통해 구조 분해 해서 각각의 변수로 만들어 활용 
+// user.age = user['age'] 
+// user라는 객체에서 구조분해를 통해 꺼내오는 방식은 user부분에 점표기법을 이용해 속성의 이름을 명시하는 것과 동일(객체데이터에서 뒤쪽에 대괄호를 적어 속성이름을 인덱싱방법으로도 꺼내서 사용)
+// 필요한 것들만 꺼내서 사용할 수 도 있음
+const user = {
   name: 'hyerin',
-  age: 85
+  age: 95,
+  email: 'abcde@gmail.com'
 }
 
-const userEmail = {
-  name: 'hyerin',
-  email: 'abcde123@gmail.com'
-}
-
-// userEmail.assign, userAge.assign 직접적으로 사용불가
-// assign메소드는 Object라는 자바스크립트 전역객체부분의 prototype으로 만들어진 메소드가 아님
-// = 정적메소드: Object라는 자바스크립트 전역객체에 직접적으로 사용
-// target부분에 source부분의 속성들을 복사해서 집어넣음
-// const target = Object.assign(userAge, userEmail) //첫번째인수userAge:대상객체, 두번째인수userEmail:출처객체
-// console.log(target) //{name: "hyerin", age: 85, email: "abcde123@gmail.com"}
-// console.log(userAge) //{name: "hyerin", age: 85, email: "abcde123@gmail.com"}
-// console.log(target === userAge) //true
-// target과 userAge는 같은 데이터, 출처객체source에서 대상객체target으로 속성의 값을 복사해서 집어넣는데 그 객체가 반환됨
+// const {name, age, email, address} = user
 
 
-// 생긴게 똑같다고 일치연산자로 같다고 나온게 아님
-// 두개의 객체는 생긴것은 똑같지만 서로 다른 객체임
-// 하나의 객체데이터는 특정한 메모리주소에 값이 들어가져있음
-// a와 b 변수는 서로 다른 메모리주소를 바라보고있으므로 같지않음
-// const a = { k: 123 }
-// const b = { k: 123 }
-// console.log(a === b) //false
+// // E.g, user.address 
+// // address는 user라는 객체데이터 내부에 존재하지 않으므로 undefined 값이 나옴
+// // 정의되어있지 않은 속성들은 꺼내온다 한들 값이 없음
 
-원본데이터는 손상하지 않고 그것의 속성들을 합쳐놓은 새로운 객체데이터를 만들어서
-반환시켜 사용가능
-userAge, userEmail을 합쳐 새로운 객체데이터 만드는방법
-const target = Object.assign({}, userAge, userEmail) //userAge, userEmail:출처객체
-console.log(target) //{name: "hyerin", age: 85, email: "abcde123@gmail.com"}
-console.log(userAge) //{name: "hyerin", age: 85}
-console.log(target === userAge) //false
+// console.log(`사용자의 이름은 ${name}입니다.`)
+// console.log(`${name}의 나이는 ${age}세입니다.`)
+// console.log(`${name}의 이메일 주소는 ${email}입니다.`)
+// console.log(address)
 
-
-userAge원본데이터를 손상하지 않고 그것을 새로운 객체데이터로 속성을 옮겨서 하나의 복사본을 만듦
-내용은 같지만 userAge 바라보고있는 메모리주소가 따로 있고 그것이 복사돼서 새로운 메모리주소로 할당되었으므로 서로 다름
-const target = Object.assign({}, userAge) 
-console.log(target) //{name: "hyerin", age: 85}
-console.log(userAge) //{name: "hyerin", age: 85}
-console.log(target === userAge) //false
+// 필요한 것들만 꺼내서 사용가능
+// const {name, age} = user
+// console.log(`사용자의 이름은 ${name}입니다.`)
+// console.log(`${name}의 나이는 ${age}세입니다.`)
+// console.log(`${name}의 이메일 주소는 ${user.email}입니다.`)
+// console.log(user.address)
 
 
-//Object.keys()
-// Object.keys() 메소드는 주어진 객체의 속성 이름들을 일반적인 반복문과 동일한 순서로 순회되는 열거할 수 있는 배열로 반환합니다.
-// name,age,email이라는 key가 Object.keys의 정적메소드를 통해 그 key들만 추출되어 새로운 배열데이터로 만들어짐
-// 속성이름만 추출해서 새로운 배열로 만들어주는 개념
+// address가 undefined라면 해당 변수 부분에 기본값을 지정할 수 있음
+// const {name, age, address = 'Korea'} = user
+// console.log(`사용자의 이름은 ${name}입니다.`)
+// console.log(`${name}의 나이는 ${age}세입니다.`)
+// console.log(`${name}의 이메일 주소는 ${user.email}입니다.`)
+// console.log(address)
+
+
+
+// 실제 user라는 객체데이터 부분에 address속성의 실제데이터가 있으면 기본값 무시됨
+// 구조분해할당이라는 문법사용시 어떤 데이터가 undefined로 비어있다면 할당연산자를 통해서 기본값 지정 가능
 // const user = {
-//   // Key: value
 //   name: 'hyerin',
-//   age: 85,
-//   email: 'abcde123@gmail.com'
+//   age: 95,
+//   email: 'abcde@gmail.com',
+//   address: 'USA'
 // }
 
-// const keys = Object.keys(user)
-// console.log(keys) //(3) ["name", "age", "email"]
+// const {name, age, address = 'Korea'} = user
+// console.log(address)
 
-// 객체데이터에서 사용할 수 있는 인덱싱방법
-// 인덱싱: 객체데이터뒤쪽에 대괄호를열고닫아 객체데이터가 가지고있는 속성이름명시(번호x)
-// user부분의 email property 값을 내놔라
-// console.log(user['email']) //abcde123@gmail.com
-// console.log(user.email) //abcde123@gmail.com
 
-// user라는 객체에서 name부분의 속성값을 가져옴
-// map메소드 콜백에서반환된 특정한데이터를 차곡차곡 배열로만들어서 반환
-// user라는 부분의 key부분의 값들을 추출해서 배열로 만들어 반환
-// const values = keys.map(key => user[key])
-console.log(values) //(3) ["hyerin", 85, "abcde123@gmail.com"]
+
+// 만약 user부분에서 name정의된 속성을 꺼내와서 사용하려고 할 때 변수를 따로만들어 다시 할당가능
+// name이라는 변수이름이 마음에 안들어서 아래쪽에서 활용할때는 다른이름으로 다시 할당받아 사용가능
+
+
+// const {name, age, address = 'Korea'} = user
+// const hyerin = name
+
+// console.log(`사용자의 이름은 ${hyerin}입니다.`) //사용자의 이름은 hyerin입니다.
+// console.log(`${hyerin}의 나이는 ${age}세입니다.`) //hyerin의 나이는 95세입니다.
+// console.log(`${hyerin}의 이메일 주소는 ${user.email}입니다.`) //hyerin의 이메일 주소는 abcde@gmail.com입니다.
+// console.log(address) //Korea
+
+// user라는 객체데이터 내부에서 어떤 속성이름으로 데이터를 꺼내올  때 속성이름이 맘에안들면 꺼내오는것은 그 속성이름으로 꺼내오지만 밑에서 변수로 활용할 때는 다른 이름으로 활용하게 콜론기호를 적어서 뒤쪽에 새로운 변수이름 명시 가능
+// 구조분해할당을 통해 문법을 작성할 때 어떤 특정한 객체에서 데이터를 꺼내올 때는 그 데이터이름을 정확하게 작성하되 그 데이터이름을 그대로 변수로 활용하고 싶지 않다면 뒤쪽에 콜론기호를 적어 새로운이름 선언가능
+const {name: hyerin, age, address = 'Korea'} = user
+
+console.log(`사용자의 이름은 ${hyerin}입니다.`) //사용자의 이름은 hyerin입니다.
+console.log(`${hyerin}의 나이는 ${age}세입니다.`) //hyerin의 나이는 95세입니다.
+console.log(`${hyerin}의 이메일 주소는 ${user.email}입니다.`) //hyerin의 이메일 주소는 abcde@gmail.com입니다.
+console.log(address)
+
+
+// 배열데이터도 구조분해할당문법 사용가능
+// 구조분해하는 데이터 종류에 맞게 기호 사용 []
+// 배열데이터같은 경우에는 item이 순서대로 indexing되어 들어가져있으므로 구조분해에서 꺼내올 때도 단순하게 순서대로 꺼내옴
+// d같은경우에는 존재하지 않는 값을 변수로 출력하려고 하다보니 undefined 값뜸
+// 배열데이터는 객체데이터와 다르게 순서대로 추출해야함
+const fruits = ['Apple', 'Banana', 'Cherry']
+// const [a, b, c, d] = fruits
+// console.log(a, b, c, d) //Apple Banana Cherry undefined
+
+// banana만 추출하고 싶을때
+// // 쉼표는 남겨둬야됨, 왜냐 배열부분에서의 구조분해는 순서대로 추출되기 때문에 순서를 명확하게 지키기 위해 쉼표를 꼭 적어야함
+// const [, b] = fruits
+// console.log(b) //Banana
+
+// // Cherry출력하고 싶을때
+const [, , b] = fruits
+console.log(b) //Cherry
