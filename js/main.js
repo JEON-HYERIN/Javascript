1d044ffb08b092005cbaa8891c46d72c77048682
JEON-HYERIN <jeonhyelin19951118@gmail.com>
2021-08-30 20:54:39 +0900

    Dream Coding : JSON

--- js/main.js
+++ js/main.js
@@ -1,49 +1,64 @@
-// Callback function using function expression
-function randomQuiz(answer, printYes, printNo) {
-  if (answer === 'love you') {
-    printYes();
-  } else {
-    printNo();
+//JSON
+//Javascript Object Notation
+
+// 1. Object to JSON
+// stringify(obj)
+let json = JSON.stringify(true);
+console.log(json); //true
+
+json = JSON.stringify(['apple', 'banana']);
+console.log(json); //["apple","banana"]
+
+const rabbit = {
+  name: 'tori',
+  color: 'white',
+  size: null,
+  birthDate: new Date(),
+  jump: () => {
+    console.log(`${this.name} can jump!`);
   }
 }
-const printYes = function () {
-  console.log('yes');
-}
 
-const printNo = function () {
-  console.log('no');
-}
+json = JSON.stringify(rabbit);
+console.log(json); //{"name":"tori","color":"white","size":null,"birthDate":"2021-08-30T11:28:40.486Z"}
 
-randomQuiz('love you',printYes,printNo); //yes
-randomQuiz('wrong',printYes,printNo); //no
+json = JSON.stringify(rabbit, ["name"]);
+console.log(json); //{"name":"tori"}
+
+json = JSON.stringify(rabbit, ["name", "color"]);
+console.log(json); //{"name":"tori","color":"white"}
+
+console.clear();
+json = JSON.stringify(rabbit, (key, value) => {
+  console.log(`key: ${key}, value: ${value}`);
+  return value;
+});
+// key: , value: [object Object]
+// key: name, value: tori
+// key: color, value: white
+// key: size, value: null
+// key: birthDate, value: 2021-08-30T11:40:13.460Z
+// key: jump, value: () => {
+//     console.log(`${this.name} can jump!`);
+//   }
+
+console.clear();
+json = JSON.stringify(rabbit, (key, value) => {
+  console.log(`key: ${key}, value: ${value}`);
+  return key === 'name' ? 'hyerin' : value;
+});
+console.log(json); //{"name":"hyerin","color":"white","size":null,"birthDate":"2021-08-30T11:42:34.018Z"}
+
+// 2. JSON to Object
+// parse(json)
+console.clear();
+json = JSON.stringify(rabbit);
+const obj = JSON.parse(json, (key, value) => {
+  console.log(`key: ${key}, value: ${value}`);
+  return key === 'birthDate' ? new Date(value) : value;
+});
+console.log(obj); //{name: "tori", color: "white", size: null, birthDate: "2021-08-30T11:44:05.353Z"}
 
 
-function calculate(command, a, b) {
-  switch (command) {
-    case 'add':
-      return a + b;
-      break;
-    case 'subtract':
-      return a - b;
-      break;
-    case 'divide':
-      return a / b;
-      break;
-    case 'multiply':
-      return a * b;
-    case 'remainder':
-      return a % b;
-    default:
-      console.log('해당없음');
-  }
-};
-
-console.log(calculate('add',2,10));
-console.log(calculate('subtract',2,2));
-console.log(calculate('divide',2,2));
-console.log(calculate('multiply',2,2));
-console.log(calculate('remainder',2,2));
-console.log(calculate('oops',2,2));
-// const add = function () {
-//   console.log(a + b);
-// }
+console.log(rabbit.birthDate.getDate());
+console.log(obj.birthDate.getDate());
